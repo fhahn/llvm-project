06eb2448d937eaade22bd76322e841469aa27c54
Jeroen Dobbelaere <jeroen.dobbelaere@synopsys.com>
2023-04-28 15:58:10 +0200

    Loop vectorizer: learn about noalias intrinsics (Based on D9382)
    [noalias] D9382: llvm.noalias - don't prevent loop vectorization
    
    Rebase of https://reviews.llvm.org/D9382

--- llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -8587,7 +8587,8 @@ VPSingleDefRecipe *VPRecipeBuilder::tryToWidenCall(CallInst *CI,
   if (ID && (ID == Intrinsic::assume || ID == Intrinsic::lifetime_end ||
              ID == Intrinsic::lifetime_start || ID == Intrinsic::sideeffect ||
              ID == Intrinsic::pseudoprobe ||
-             ID == Intrinsic::experimental_noalias_scope_decl))
+             ID == Intrinsic::experimental_noalias_scope_decl ||
+             ID == Intrinsic::noalias || ID == Intrinsic::provenance_noalias))
     return nullptr;
 
   SmallVector<VPValue *, 4> Ops(Operands.take_front(CI->arg_size()));
