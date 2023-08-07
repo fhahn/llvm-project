29ccba83d0b5a04211719763efbc39fe5f6fd000
Florian Hahn <flo@fhahn.com>
2023-08-07 12:30:37 +0100

    Remove expensive attrs

--- llvm/lib/Transforms/IPO/Attributor.cpp
+++ llvm/lib/Transforms/IPO/Attributor.cpp
@@ -3825,7 +3825,7 @@ static bool runAttributorLightOnFunctions(InformationCache &InfoCache,
        &AANoFree::ID, &AANoReturn::ID, &AAMemoryLocation::ID,
        &AAMemoryBehavior::ID, &AAUnderlyingObjects::ID, &AANoCapture::ID,
        &AAInterFnReachability::ID, &AAIntraFnReachability::ID, &AACallEdges::ID,
-       &AANoFPClass::ID, &AAMustProgress::ID, &AANonNull::ID});
+       &AAMustProgress::ID});
   AC.Allowed = &Allowed;
   AC.UseLiveness = false;
 
