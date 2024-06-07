a8dd6d38f78d3eb077cbd6f01c2988597ad0200b
Florian Hahn <flo@fhahn.com>
2024-06-07 21:53:35 +0100

    tep

--- llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
@@ -1602,15 +1602,30 @@ void VPlanTransforms::tryToRealignLoop(VPlan &Plan) {
   }
 
   VPBuilder BPH(cast<VPBasicBlock>(VectorLoop->getSinglePredecessor()));
-  VPValue *RemIters = B.createNaryOp(Instruction::URem, {Plan.getTripCount(), Plan.getVFxUF()});
-  VPValue *NeedsAlign = B.createICmp(CmpInst::ICMP_EQ, RemIters, ConstantInt::get(Plan.getCanonicalIV()->getScalarType(), 0));
+  VPValue *RemIters = BPH.createNaryOp(Instruction::URem, {Plan.getTripCount(), &Plan.getVFxUF()}, {}, "rem.iters");
+  VPValue *NeedsAlign = BPH.createICmp(CmpInst::ICMP_NE, RemIters, Plan.getOrAddLiveIn(ConstantInt::get(Plan.getCanonicalIV()->getScalarType(), 0)), {}, "needs.align");
+  VPValue *Offset = BPH.createNaryOp(Instruction::Sub, {&Plan.getVFxUF(), RemIters}, {}, "offset");
+  VPValue *ResetCnt = BPH.createNaryOp(Instruction::Sub, {Plan.getTripCount(), &Plan.getVFxUF()}, {}, "reset.cnt");
 
-  VPBuilder B(&*Header->getFirstNonPhi());
-  VPValue *IsLastIter = B.createICmp(CmpInst::ICMP_NE, Plan.getCanonicalIV(), Plan.getVectorTripCount());
-  VPValue *AlignIter = B.createNaryOp(Instruction::And, {NeedsAlign, IsLasteIter});
-  VPValue *Sel = B.createSelect(Cmp, CanIV, RemIters);
+  VPInstruction *Sub = BPH.createNaryOp(Instruction::Sub, {&Plan.getVFxUF(), Plan.getOrAddLiveIn(ConstantInt::get(Plan.getCanonicalIV()->getScalarType(), 1))}, {}, "rnd.up.tc");
+  VPInstruction *RndUpVecTC1 = BPH.createNaryOp(Instruction::Add, {Plan.getTripCount(), Sub}, {}, "rnd.up.tc");
+  VPInstruction *RndUpVecTC2 = BPH.createNaryOp(Instruction::URem, {RndUpVecTC1, &Plan.getVFxUF()}, {}, "rnd.up.tc");
+  VPInstruction *RndUpVecTC = BPH.createNaryOp(Instruction::Sub, {RndUpVecTC1, RndUpVecTC2}, {}, "rnd.up.tc");
+  Plan.getVectorTripCount().replaceAllUsesWith(RndUpVecTC);
 
-  CanIV->replaceUsesWithIf(Sel, [CanIV, Cmp, RemIters, Sel](VPUser &U, unsigned) {
-                           return !isa<VPInstruction>(&U) || (cast<VPInstruction>(&U) !=  CanIV->getBackedgeValue() && cast<VPInstruction>(&U) != Cmp && cast<VPInstruction>(&U) != RemIters&& cast<VPInstruction>(&U) != Sel);
+
+  auto *IVInc = cast<VPInstruction>(Plan.getCanonicalIV()->getBackedgeValue());
+  IVInc->moveBefore(*Header, Header->getFirstNonPhi());
+  VPBuilder B(IVInc->getNextNode());
+  VPValue *IsLastIter = B.createICmp(CmpInst::ICMP_EQ, IVInc, RndUpVecTC);
+  VPValue *AlignIter = B.createNaryOp(Instruction::And, {NeedsAlign, IsLastIter});
+  VPValue *Sel = B.createSelect(AlignIter, ResetCnt, CanIV);
+
+  CanIV->replaceUsesWithIf(Sel, [CanIV, RemIters, Sel](VPUser &U, unsigned) {
+                           return !isa<VPInstruction>(&U) || (cast<VPInstruction>(&U) !=  CanIV->getBackedgeValue() && cast<VPInstruction>(&U) != RemIters&& cast<VPInstruction>(&U) != Sel);
                            });
+
+   auto *MiddleBB = cast<VPBasicBlock>(VectorLoop->getSingleSuccessor());
+   auto *BranchOnCond = cast<VPInstruction>(MiddleBB->getTerminator());
+   BranchOnCond->setOperand(0, Plan.getOrAddLiveIn(ConstantInt::getTrue(Plan.getCanonicalIV()->getScalarType()->getContext())));
 }
