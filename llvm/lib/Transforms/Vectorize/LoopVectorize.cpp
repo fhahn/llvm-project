9811f95c2700c12601db9247d52656e3dbdaac38
Florian Hahn <flo@fhahn.com>
2024-06-11 12:16:02 +0100

    Step

--- llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -7441,6 +7441,9 @@ LoopVectorizationPlanner::executePlan(
     LoopVectorizeHints Hints(L, true, *ORE);
     Hints.setAlreadyVectorized();
   }
+    assert(State.CFG.DTU.getDomTree().verify(
+        DominatorTree::VerificationLevel::Fast) &&
+      "DT not preserved correctly");
   TargetTransformInfo::UnrollingPreferences UP;
   TTI.getUnrollingPreferences(L, *PSE.getSE(), UP, ORE);
   if (!UP.UnrollVectorizedLoop || CanonicalIVStartValue)
@@ -10223,7 +10226,16 @@ LoopVectorizeResult LoopVectorizePass::runImpl(
     // transform.
     Changed |= formLCSSARecursively(*L, *DT, LI, SE);
 
+    assert(DT->verify(
+        DominatorTree::VerificationLevel::Fast) &&
+      "DT not preserved correctly");
+
+
     Changed |= CFGChanged |= processLoop(L);
+
+    assert(DT->verify(
+        DominatorTree::VerificationLevel::Fast) &&
+      "DT not preserved correctly");
 
     if (Changed) {
       LAIs->clear();
