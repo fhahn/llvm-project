894b3fc9e13666aa58ab18a77aba013aa9144654
Florian Hahn <flo@fhahn.com>
2024-10-12 21:40:00 +0100

    Step

--- llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
@@ -973,9 +973,20 @@ static void simplifyRecipe(VPRecipeBase &R, VPTypeAnalysis &TypeInfo) {
   if (auto *DerivedIV = dyn_cast<VPDerivedIVRecipe>(&R)) {
     VPValue *Start = DerivedIV->getStartValue();
     VPValue *Step = DerivedIV->getStepValue();
+    VPValue *Op = DerivedIV->getOperand(1);
     if (Step->isLiveIn() && Start->isLiveIn()) {
       auto *StartC = dyn_cast<ConstantInt>(Start->getLiveInIRValue());
       auto *StepC = dyn_cast<ConstantInt>(Step->getLiveInIRValue());
+      if (Op->isLiveIn()) {
+        auto *OpC = dyn_cast_if_present<ConstantInt>(Op->getUnderlyingValue());
+        if (OpC && OpC->getType() == StepC->getType() && StartC->getType()) {
+          APInt Folded =
+              StartC->getValue() + OpC->getValue() * StepC->getValue();
+          DerivedIV->replaceAllUsesWith(
+              R.getParent()->getPlan()->getOrAddLiveIn(
+                  ConstantInt::get(TypeInfo.getContext(), Folded)));
+        }
+      }
       if (StartC && StartC->isZero() && StepC && StepC->isOne() && TypeInfo.inferScalarType(DerivedIV) == TypeInfo.inferScalarType(DerivedIV->getOperand(1))) {
         DerivedIV->replaceAllUsesWith(DerivedIV->getOperand(1)); 
         DerivedIV->eraseFromParent();
