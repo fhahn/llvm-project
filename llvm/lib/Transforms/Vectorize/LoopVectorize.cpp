04a44f75623395069334b8c9a0f2579a69a0af66
Florian Hahn <flo@fhahn.com>
2025-06-19 19:13:18 +0100

    Limit interleaving, min profitable threshold.

--- llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -4814,6 +4814,13 @@ LoopVectorizationCostModel::selectInterleaveCount(VPlan &Plan, ElementCount VF,
       MaxInterleaveCount = ForceTargetMaxVectorInterleaveFactor;
   }
 
+  for (auto &P : Plan.getVectorLoopRegion()->getEntryBasicBlock()->phis()) {
+    auto *RedP = dyn_cast<VPReductionPHIRecipe>(&P);
+    if (RedP && RedP->getRecurrenceKind() == RecurKind::FMaxNoNaNs) {
+        MaxInterleaveCount = std::min(MaxInterleaveCount, 2u);
+    }
+  }
+
   unsigned EstimatedVF = getEstimatedRuntimeVF(VF, VScaleForTuning);
 
   // Try to get the exact trip count, or an estimate based on profiling data or
@@ -9755,7 +9762,7 @@ static InstructionCost calculateEarlyExitCost(VPCostContext &CostCtx,
 ///     extra work when exiting the loop early, such as calculating the final
 ///     exit values of variables used outside the loop.
 static bool isOutsideLoopWorkProfitable(GeneratedRTChecks &Checks,
-                                        VectorizationFactor &VF, Loop *L,
+                                        VectorizationFactor &VF, unsigned UF, Loop *L,
                                         PredicatedScalarEvolution &PSE,
                                         VPCostContext &CostCtx, VPlan &Plan,
                                         ScalarEpilogueLowering SEL,
@@ -9843,6 +9850,15 @@ static bool isOutsideLoopWorkProfitable(GeneratedRTChecks &Checks,
       dbgs() << "LV: Minimum required TC for runtime checks to be profitable:"
              << VF.MinProfitableTripCount << "\n");
 
+
+  for (auto &P : Plan.getVectorLoopRegion()->getEntryBasicBlock()->phis()) {
+    auto *RedP = dyn_cast<VPReductionPHIRecipe>(&P);
+    if (RedP && RedP->getRecurrenceKind() == RecurKind::FMaxNoNaNs) {
+      auto X = VF.Width.multiplyCoefficientBy(2 * UF);
+      if (ElementCount::isKnownLT(VF.MinProfitableTripCount, X))
+        VF.MinProfitableTripCount = X;
+    }
+  }
   // Skip vectorization if the expected trip count is less than the minimum
   // required trip count.
   if (auto ExpectedTC = getSmallBestKnownTC(PSE, L)) {
@@ -10320,7 +10336,7 @@ bool LoopVectorizePass::processLoop(Loop *L) {
     VPCostContext CostCtx(CM.TTI, *CM.TLI, CM.Legal->getWidestInductionType(),
                           CM, CM.CostKind);
     if (!ForceVectorization &&
-        !isOutsideLoopWorkProfitable(Checks, VF, L, PSE, CostCtx,
+        !isOutsideLoopWorkProfitable(Checks, VF, IC, L, PSE, CostCtx,
                                      LVP.getPlanFor(VF.Width), SEL,
                                      CM.getVScaleForTuning())) {
       ORE->emit([&]() {
