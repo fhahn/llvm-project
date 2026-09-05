7a9671bc40a199a725a0c74703bfc7ba9b290ee5
Florian Hahn <flo@fhahn.com>
2026-09-05 22:05:39 +0100

    Worklist 128

--- llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
@@ -1665,7 +1665,7 @@ static VPValue *simplifyRecipe(VPSingleDefRecipe *Def) {
 }
 
 void VPlanTransforms::simplifyRecipes(VPlan &Plan) {
-  SmallVector<VPSingleDefRecipe *, 64> Worklist;
+  SmallVector<VPSingleDefRecipe *, 128> Worklist;
   PostOrderTraversal<VPBlockDeepTraversalWrapper<VPBlockBase *>> POT(
       Plan.getEntry());
   for (VPBasicBlock *VPBB : VPBlockUtils::blocksOnly<VPBasicBlock>(POT))
