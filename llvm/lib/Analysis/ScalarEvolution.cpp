4b4a672ddd666b399baf4af3fd707711986448ff
Florian Hahn <flo@fhahn.com>
2026-09-19 20:04:00 +0100

    [SCEV] Update SCEVUse vector inline storage.

--- llvm/lib/Analysis/ScalarEvolution.cpp
+++ llvm/lib/Analysis/ScalarEvolution.cpp
@@ -16464,7 +16464,7 @@ const SCEV *ScalarEvolution::LoopGuards::rewrite(const SCEV *Expr) const {
             return SE.getAddExpr(S, SE.getMinusOne(Expr->getType()));
         }
       }
-      SmallVector<SCEVUse, 2> Operands;
+      SmallVector<SCEVUse, 4> Operands;
       bool Changed = false;
       for (SCEVUse Op : Expr->operands()) {
         Operands.push_back(
@@ -16480,7 +16480,7 @@ const SCEV *ScalarEvolution::LoopGuards::rewrite(const SCEV *Expr) const {
     }
 
     const SCEV *visitMulExpr(const SCEVMulExpr *Expr) {
-      SmallVector<SCEVUse, 2> Operands;
+      SmallVector<SCEVUse, 4> Operands;
       bool Changed = false;
       for (SCEVUse Op : Expr->operands()) {
         Operands.push_back(
